ba633b7f8dd545892609610beb83f14d58016ba2
Martina Velardi <m.velardi.1394@vallauri.edu>
2021-09-24 10:44:13 +0200

    Completato esercizio 02

---
+++ 02_SQL02/02_SQL02/query.sql
@@ -0,0 +1,17 @@
+﻿/* Le città da cui partono voli per Roma */
+SELECT DISTINCT v.CittaPart
+FROM Volo v
+WHERE v.CittaArr='Roma'
+ORDER BY v.CittaPart
+
+/* Città con aeroporto con numero di piste non noto */
+SELECT a.Citta
+FROM Aeroporto a
+WHERE a.NumPiste IS NULL
+
+/* Per ogni volo misto (sia merci che passeggeri): codice volo e dati di trasporto */
+SELECT *
+FROM Aereo a, Volo v
+WHERE a.TipoAereo=v.TipoAereo
+AND a.QtaMerci>0
+AND a.NumPasseggeri>0
