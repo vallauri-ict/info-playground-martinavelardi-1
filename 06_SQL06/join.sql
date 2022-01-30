4926a2bc1a5161db1f5066e54885d93345361c36
Martina Velardi <m.velardi.1394@vallauri.edu>
2022-01-30 21:51:50 +0100

    caricato es06

---
+++ 06_SQL06/join.sql
@@ -0,0 +1,11 @@
+﻿SELECT * 
+FROM Nazione n INNER JOIN Continente c
+ON n.codContinente = c.idContinente
+
+SELECT * 
+FROM Nazione n LEFT JOIN Continente c
+ON n.codContinente = c.idContinente
+
+SELECT * 
+FROM Nazione n RIGHT JOIN Continente c
+ON n.codContinente = c.idContinente
