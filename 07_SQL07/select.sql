4b9dda7a441e61ff06227591f9a68c0578570a24
Martina Velardi <m.velardi.1394@vallauri.edu>
2022-01-30 21:52:37 +0100

    caricato es07

---
+++ 07_SQL07/select.sql
@@ -0,0 +1,14 @@
+﻿SELECT 'Negozio' AS Tipo, [Data] FROM VenditaNegozio
+UNION
+SELECT 'Internet' AS Tipo, [Data] FROM VenditaInternet
+ORDER BY [Data] DESC;
+
+SELECT [Data] FROM VenditaNegozio
+INTERSECT
+SELECT [Data] FROM VenditaInternet
+ORDER BY [Data] DESC;
+
+SELECT [Data] FROM VenditaNegozio
+EXCEPT
+SELECT [Data] FROM VenditaInternet
+ORDER BY [Data] DESC;
